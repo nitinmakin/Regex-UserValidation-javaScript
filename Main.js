0c293638a942903745130813e2a6b89a89c22d67
nitinmakin <nitinmakin124.com>
2020-09-27 14:09:39 +0530

    Added(UC3)-to validate email

--- Main.js
+++ Main.js
@@ -4,9 +4,9 @@ console.log("Welcome to user validation Program")
 var input = readline.createInterface(process.stdin, process.stdout);
 var i = 0;
 
-input.question("1.First_Name   2.Last_Name : ", function (option) {
+input.question("1.First_Name   2.Last_Name  3.Email : ", function (option) {
 
-    switch (option) {     
+    switch (option) {
         case "1":
             input.question("Enter First Name: ", function (firstName) {
                 result.validateFirstName(firstName);
@@ -17,6 +17,13 @@ input.question("1.First_Name   2.Last_Name : ", function (option) {
         case "2":
             input.question("Enter Last Name: ", function (lastName) {
                 result.validateFirstName(lastName);
+                process.exit();
+            })
+            break;
+
+        case "3":
+            input.question("Enter Your Email: ", function (Email) {
+                result.validateEmail(Email);
                 process.exit();
             })
             break;
--- Utility.js
+++ Utility.js
@@ -4,9 +4,15 @@ class UserValidation {
         let firstName = /^[A-Z]{1}([a-z]{2,15})$/.test(Name)
         console.log(firstName);
     }
+
     validateLastName = (Name) => {
         let firstName = /^[A-Z]{1}([a-z]{2,15})$/.test(Name)
         console.log(firstName);
+    }
+
+    validateEmail = (Email) => {
+        let eMail = /^[a-zA-Z]+[a-zA-Z0-9]*[._+-]?[a-zA-Z0-9]*@[A-Za-z0-9]+([.][a-zA-Z]{2,})$/.test(Email)
+        console.log(eMail);
     }
 }
 module.exports = new UserValidation();
