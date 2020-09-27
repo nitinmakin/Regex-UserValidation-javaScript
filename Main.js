dfebd9e7770033be4a0246638c817592f9d05245
nitinmakin <nitinmakin124.com>
2020-09-27 13:56:07 +0530

    Added(UC2)-to validate Last Name

--- Main.js
+++ Main.js
@@ -2,18 +2,30 @@ const result = require('./Utility')
 var readline = require('readline');
 console.log("Welcome to user validation Program")
 var input = readline.createInterface(process.stdin, process.stdout);
-input.question("1.First Name  2.Last Name", function (option) {
+var i = 0;
 
-    switch (option) {
+input.question("1.First_Name   2.Last_Name : ", function (option) {
+
+    switch (option) {     
         case "1":
             input.question("Enter First Name: ", function (firstName) {
                 result.validateFirstName(firstName);
                 process.exit();
-                      })
-                      break;
-        }
-    
+            })
+            break;
+
+        case "2":
+            input.question("Enter Last Name: ", function (lastName) {
+                result.validateFirstName(lastName);
+                process.exit();
+            })
+            break;
+
+    }
+
+
 })
+
 
 
 
--- Utility.js
+++ Utility.js
@@ -1,6 +1,10 @@
 class UserValidation {
-    
+
     validateFirstName = (Name) => {
+        let firstName = /^[A-Z]{1}([a-z]{2,15})$/.test(Name)
+        console.log(firstName);
+    }
+    validateLastName = (Name) => {
         let firstName = /^[A-Z]{1}([a-z]{2,15})$/.test(Name)
         console.log(firstName);
     }
